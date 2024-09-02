e6f880cf0e367cfd2b5c66903c2b625911a25908
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2024-09-03 01:53:58 +0200

    UnitFrame: restrict UnitFrameManaBar_UpdateType hooksecurefunc

--- skins/unitframes/UnitFrame.lua
+++ skins/unitframes/UnitFrame.lua
@@ -563,44 +563,34 @@ function CfUnitFrameManaBar_Update(statusbar, unit)
 end
 
 hooksecurefunc("UnitFrameManaBar_UpdateType", function(manaBar)
-	if ( not manaBar ) then
-		return
+	if not manaBar or (manaBar.unit ~= "targettarget" and manaBar.unit ~= "focustarget") then
+		return;
 	end
-
 	local powerType, powerToken, altR, altG, altB = UnitPowerType(manaBar.unit)
-	local info = CfPowerBarColor[powerToken]
-
-	manaBar:SetStatusBarTexture("Interface\\AddOns\\ClassicFrames\\textures\\UI-StatusBar")
-
+	local info = CfPowerBarColor[powerToken];
 	if ( info ) then
-		local playerDeadOrGhost = (manaBar.unit == "player" and (UnitIsDead("player") or UnitIsGhost("player")))
 		if ( info.atlas ) then
 			manaBar:SetStatusBarTexture(info.atlas)
 			manaBar:SetStatusBarColor(1, 1, 1)
-			manaBar:GetStatusBarTexture():SetDesaturated(playerDeadOrGhost)
-			manaBar:GetStatusBarTexture():SetAlpha(playerDeadOrGhost and 0.5 or 1)
 		else
-			if ( playerDeadOrGhost ) then
-				manaBar:SetStatusBarColor(0.6, 0.6, 0.6, 0.5)
-			else
-				manaBar:SetStatusBarColor(info.r, info.g, info.b)
-			end
+			manaBar:SetStatusBarTexture("Interface\\AddOns\\ClassicFrames\\textures\\UI-StatusBar")
+			manaBar:SetStatusBarColor(info.r, info.g, info.b)
 		end
-		if (manaBar.FeedbackFrame) then
-			if (info.atlas) then
-				manaBar.FeedbackFrame.BarTexture:SetAtlas(info.atlas, false)
-			else
-				manaBar.FeedbackFrame.BarTexture:SetVertexColor(info.r, info.g, info.b)
-			end
-		end
-		if (manaBar.Spark) then
+		if ( manaBar.Spark ) then
 			manaBar.Spark:SetAlpha(0)
 		end
 	else
 		if ( not altR ) then
-			info = CfPowerBarColor[powerType] or CfPowerBarColor["MANA"]
+			info = CfPowerBarColor[powerType] or CfPowerBarColor["MANA"];
 		else
-			manaBar:SetStatusBarColor(altR, altG, altB)
+			if ( not manaBar.lockColor ) then
+				manaBar:SetStatusBarColor(altR, altG, altB)
+			end
 		end
+	end
+	if ( manaBar.powerType ~= powerType ) then
+		manaBar.powerType = powerType;
+		manaBar.powerToken = powerToken;
+		manaBar.currValue = UnitPower("player", powerType)
 	end
 end)
