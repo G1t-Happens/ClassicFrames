6f4502423a135ad6d818045330862a4831d7b63c
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-02-09 19:58:47 +0100

    add: groupindicator style

--- skins/unitframes/PlayerFrame.lua
+++ skins/unitframes/PlayerFrame.lua
@@ -37,6 +37,29 @@ PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarsContainer:Hide()
 PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.ManaBarArea:Hide()
 PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator:Hide()
 
+local groupIndicator = PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator
+if groupIndicator then
+	groupIndicator.GroupIndicatorLeft:SetSize(24, 16)
+	groupIndicator.GroupIndicatorLeft:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+	groupIndicator.GroupIndicatorLeft:SetTexCoord(0, 0.1875, 0, 1)
+	groupIndicator.GroupIndicatorLeft:SetAlpha(0.3)
+	groupIndicator.GroupIndicatorRight:SetSize(24, 16)
+	groupIndicator.GroupIndicatorRight:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+	groupIndicator.GroupIndicatorRight:SetTexCoord(0.53125, 0.71875, 0, 1)
+	groupIndicator.GroupIndicatorRight:SetAlpha(0.3)
+	if (groupIndicator.GroupIndicatorMiddle == nil) then
+		groupIndicator.GroupIndicatorMiddle = groupIndicator:CreateTexture(nil, "BACKGROUND")
+		groupIndicator.GroupIndicatorMiddle:SetSize(0, 16)
+		groupIndicator.GroupIndicatorMiddle:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+		groupIndicator.GroupIndicatorMiddle:SetTexCoord(0.1875, 0.53125, 0, 1)
+		groupIndicator.GroupIndicatorMiddle:SetPoint("LEFT", groupIndicator.GroupIndicatorLeft, "RIGHT")
+		groupIndicator.GroupIndicatorMiddle:SetPoint("RIGHT", groupIndicator.GroupIndicatorRight, "LEFT")
+		groupIndicator.GroupIndicatorMiddle:SetAlpha(0.3)
+	end
+	select(3, groupIndicator:GetRegions()):SetAlpha(0)
+	PlayerFrameGroupIndicatorText:SetPoint("LEFT", 20, -2)
+end
+
 if (PlayerFrame.nameBackground == nil) then
 	PlayerFrame.nameBackground = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain:CreateTexture(nil, "BACKGROUND")
 	PlayerFrame.nameBackground:SetSize(118, 19)
