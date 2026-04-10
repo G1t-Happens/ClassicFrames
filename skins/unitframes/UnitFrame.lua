bf14283c6d39607db8800909d99b6bd75e80c433
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2026-04-10 10:02:09 +0200

    remove + update textures

--- skins/unitframes/UnitFrame.lua
+++ skins/unitframes/UnitFrame.lua
@@ -7,7 +7,7 @@ local hooksecurefunc = hooksecurefunc
 local UnitPowerType = UnitPowerType
 
 -- Default status bar texture
-local DEFAULT_STATUSBAR_TEX = "Interface\\TargetingFrame\\UI-StatusBar"
+local DEFAULT_STATUSBAR_TEX = "Interface\\AddOns\\ClassicFrames\\textures\\UI-StatusBar"
 
 local powerBarColor = {
     MANA           = { r = 0.00, g = 0.00, b = 1.00 },
@@ -18,18 +18,17 @@ local powerBarColor = {
     RUNES          = { r = 0.50, g = 0.50, b = 0.50 },
     RUNIC_POWER    = { r = 0.00, g = 0.82, b = 1.00 },
     SOUL_SHARDS    = { r = 0.50, g = 0.32, b = 0.55 },
-    LUNAR_POWER    = { r = 0.30, g = 0.52, b = 0.90, atlas = "_Druid-LunarBar" },
+    LUNAR_POWER    = { r = 0.30, g = 0.52, b = 0.90 },
     HOLY_POWER     = { r = 0.95, g = 0.90, b = 0.60 },
-    MAELSTROM      = { r = 0.00, g = 0.50, b = 1.00, atlas = "_Shaman-MaelstromBar" },
-    INSANITY       = { r = 0.40, g = 0.00, b = 0.80, atlas = "_Priest-InsanityBar" },
+    MAELSTROM      = { r = 0.00, g = 0.50, b = 1.00 },
+    INSANITY       = { r = 0.40, g = 0.00, b = 0.80 },
     CHI            = { r = 0.71, g = 1.00, b = 0.92 },
     ARCANE_CHARGES = { r = 0.10, g = 0.10, b = 0.98 },
-    FURY           = { r = 0.788, g = 0.259, b = 0.992, atlas = "_DemonHunter-DemonicFuryBar" },
-    PAIN           = { r = 1.00, g = 156 / 255, b = 0.00, atlas = "_DemonHunter-DemonicPainBar" },
+    FURY           = { r = 0.788, g = 0.259, b = 0.992 },
+    PAIN           = { r = 1.00, g = 156 / 255, b = 0.00 },
     -- Vehicle
     AMMOSLOT       = { r = 0.80, g = 0.60, b = 0.00 },
     FUEL           = { r = 0.00, g = 0.55, b = 0.50 },
-    STAGGER        = { { r = 0.52, g = 1.00, b = 0.52 }, { r = 1.00, g = 0.98, b = 0.72 }, { r = 1.00, g = 0.42, b = 0.42 } },
 }
 
 -- Numeric fallback indices (shared references, no duplication)
@@ -52,18 +51,11 @@ local fallbackMana = powerBarColor.MANA
 
 hooksecurefunc("UnitFrameManaBar_UpdateType", function(manaBar)
     if not manaBar then return end
-
     local powerType, powerToken, altR, altG, altB = UnitPowerType(manaBar.unit)
     local info = powerBarColor[powerToken]
-
     if info then
-        if info.atlas then
-            manaBar:SetStatusBarTexture(info.atlas)
-            manaBar:SetStatusBarColor(1, 1, 1)
-        else
-            manaBar:SetStatusBarTexture(DEFAULT_STATUSBAR_TEX)
-            manaBar:SetStatusBarColor(info.r, info.g, info.b)
-        end
+        manaBar:SetStatusBarTexture(DEFAULT_STATUSBAR_TEX)
+        manaBar:SetStatusBarColor(info.r, info.g, info.b)
         local spark = manaBar.Spark
         if spark then spark:SetAlpha(0) end
     elseif altR then
@@ -71,12 +63,7 @@ hooksecurefunc("UnitFrameManaBar_UpdateType", function(manaBar)
         manaBar:SetStatusBarColor(altR, altG, altB)
     else
         info = powerBarColor[powerType] or fallbackMana
-        if info.atlas then
-            manaBar:SetStatusBarTexture(info.atlas)
-            manaBar:SetStatusBarColor(1, 1, 1)
-        else
-            manaBar:SetStatusBarTexture(DEFAULT_STATUSBAR_TEX)
-            manaBar:SetStatusBarColor(info.r, info.g, info.b)
-        end
+        manaBar:SetStatusBarTexture(DEFAULT_STATUSBAR_TEX)
+        manaBar:SetStatusBarColor(info.r, info.g, info.b)
     end
 end)
