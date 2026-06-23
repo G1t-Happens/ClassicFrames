601d195cbd4df4037984ca96cfb479ca3eada403
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2026-06-23 15:47:41 +0200

    update RaidFrame.lua

--- skins/RaidFrame.lua
+++ skins/RaidFrame.lua
@@ -2,10 +2,13 @@
 -- RaidFrame.lua
 -- =============================================================================
 
+-- Cached globals
+local hooksecurefunc = hooksecurefunc
+
 hooksecurefunc("CompactUnitFrame_SetUpFrame", function(frame)
-    if not frame or frame:IsForbidden() then return end
-    local name = frame.name
+    local name = frame and frame.name
     if not name or name._nameHidden then return end
+    if frame:IsForbidden() then return end
     name:SetAlpha(0)
     name._nameHidden = true
 end)
